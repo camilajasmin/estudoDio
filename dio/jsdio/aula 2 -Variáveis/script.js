559f38494b27cf07afe51922abb5431db04af177
camila espinoza <camilajasmin.ec@gmail.com>
2024-11-07 15:53:00 -0300

    Aulas JS básico dio (aula 1, 2 e 3)

---
+++ dio/jsdio/aula 2 -Variáveis/script.js
@@ -0,0 +1,63 @@
+// // tipos primitivos
+
+// // boolean
+// var verdadeiro_falso = false;
+// console.log(typeof(verdadeiro_falso));
+
+// // number
+// var num = 45;
+// console.log(num);
+// console.log(typeof(num));
+
+// // string
+// var nome = "camila";
+// console.log(nome);
+// console.log(typeof(nome));
+
+// ESCOPO GLOBAL E LOCAL
+
+// var escopoGlobal = 'global';
+// console.log(escopoGlobal);
+
+// function escopoLocal() {
+//     let escopoLocalInterno = 'local';
+// }
+
+// console.log(escopoLocalInterno)
+
+// OPERADORES ARITMÉTICOS
+
+// var soma = 1 + 2;
+// console.log(soma);
+
+// var subtracao = 6 - 2;
+// console.log(subtracao);
+
+// var multi = 2*3;
+// console.log(multi);
+
+// var divisaoReal = 10/3;
+// console.log(divisaoReal);
+
+// // a divisão inteira com o símbolo % mostra o resto, o que sobra
+// var divisaoInt = 7%2;
+// console.log(divisaoInt);
+
+// var potenciacao = 2**3;
+// console.log(potenciacao);
+
+
+// OPERADORES RELACIONAIS
+
+// var maior_que = 5>2;
+// console.log(maior_que);
+
+// var menor_que = 2<5;
+// console.log(menor_que);
+
+// var maiorQue = 3>6;
+// console.log(maiorQue);
+
+// var menorQue = 10<5;
+// console.log(menorQue);
+
---
+++ dio/jsdio/aula 3 - Arrays e Objetos/script.js
@@ -0,0 +1,2 @@
+let array = ['texto', 1, true, false, ['array1'], ['array2']];
+console.log(array);

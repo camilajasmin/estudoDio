9d08e5bbce712315ed94fe1d67cbf10be4c71299
camila espinoza <camilajasmin.ec@gmail.com>
2024-11-11 19:46:32 -0300

    Aula três e exercícios e contribuição

--- dio/jsdio/aula 3 - Arrays e Objetos/script.js
+++ dio/jsdio/aula 3 - Arrays e Objetos/script.js
@@ -1,2 +1,33 @@
 let array = ['texto', 1, true, false, ['array1'], ['array2']];
-console.log(array);
+// console.log(array);
+
+// // forEach
+// array.forEach(function(item, index){console.log(item, index)});
+
+// // push
+// array.push(123);
+// console.log(array);
+
+// // pop
+// array.pop();
+// console.log(array);
+
+// // shift
+// array.shift();
+// console.log(array);
+
+// // unshift
+// array.unshift('carina bianca');
+// console.log(array);
+
+// indexOf
+// console.log(array.indexOf(true));
+
+let object = {
+    string: 'string', 
+    number: 1,
+    boolean: true,
+    array: ['array'],
+    objectInterno:{objectInterno: 'objeto interno'}
+};
+console.log(object.number);

e3955557ae6d0dff98a5601ad16e6bd05406ba4a
camila espinoza <camilajasmin.ec@gmail.com>
2024-11-11 19:59:36 -0300

    Aula 4, aula sobre estruturas condicionais

---
+++ dio/jsdio/aula 4 -  Estruturas Condicionais/script.js
@@ -0,0 +1,13 @@
+var jogador1 = 0;
+var jogador2 = 3;
+var placar;
+
+if (jogador1 > 0){
+    console.log('Jocador 1 marcou ponto')
+}
+else if(jogador2 > 0){
+    console.log('Jogador 2 marcou ponto')
+}
+else{
+    console.log('Ningém marcou ponto')
+}

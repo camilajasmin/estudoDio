c8de59a19ed83e7deb9f189ad2254ed115c00003
camila espinoza <camilajasmin.ec@gmail.com>
2024-11-11 21:00:13 -0300

    aula 5, aula sobre funções

---
+++ dio/jsdio/aula 5 - Funções/script.js
@@ -0,0 +1,15 @@
+// function func() {
+//     console.log("Tudo certo")
+// }
+
+// func();
+
+// CRIANDO E DECLARANDO FUNÇÕES COM PARÂMETROS
+
+// criando a função com o function
+function mensagem(primeiro, segundo){
+    console.log(primeiro, segundo)
+}
+
+// chamando a função e usando parâmetros
+mensagem('camila', 'carina');
